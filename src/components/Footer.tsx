91c6a9b328759a1f0837ce7cb86769e4a2081a09
Wellington Jorge <wjc.rio.04@gmail.com>
2025-06-03 11:46:49 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -67,7 +67,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center">
           <p className="text-gray-400 font-poppins">
-            &copy; 2024 Casa Premium. Todos os direitos reservados.
+            &copy; 2024 EmpoRio. Todos os direitos reservados.
           </p>
         </div>
       </div>
