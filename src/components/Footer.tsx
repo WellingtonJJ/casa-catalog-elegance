22a05d3e26f576f507f1f17c494c423e7af3bc9b
Wellington Jorge <wjc.rio.04@gmail.com>
2025-06-03 11:44:39 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -55,11 +55,11 @@ const Footer = () => {
               </div>
               <div className="flex items-center text-gray-300">
                 <i className="fas fa-envelope mr-3 text-gold-400"></i>
-                <span>contato@casapremium.com</span>
+                <span>contato@emporio.com</span>
               </div>
               <div className="flex items-start text-gray-300">
                 <i className="fas fa-map-marker-alt mr-3 text-gold-400 mt-1"></i>
-                <span>São Paulo, SP<br />Brasil</span>
+                <span>Rio de Janeiro, RJ<br />Brasil</span>
               </div>
             </div>
           </div>
