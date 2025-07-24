664bc2738d9e247571272e5810b6b4f12d751bce
Wellington Jorge <wjc.rio.04@gmail.com>
2025-07-24 13:41:01 -0300

    Updated HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -81,7 +81,7 @@ const HeroSection = () => {
                 <h1 className="font-playfair text-4xl md:text-5xl lg:text-7xl font-bold text-white mb-4 md:mb-6 animate-fade-in">
                   {slide.title}
                   {slide.subtitle && (
-                    <span className="">
+                    <span className=" text-gold-400 text-2xl md:text-3xl lg:text-4xl mt-2">
                       {slide.subtitle}
                     </span>
                   )}
